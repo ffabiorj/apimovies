a3fa43c2bbc7c167d36c0e18ecdfa7fd32609af2
Fabio Oliveira <fabio20rj@gmail.com>
2019-04-06 21:08:17 -0300

    Criando url da apis de filmes

--- app/api.py
+++ app/api.py
@@ -1,4 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from app.models import Movie, Cast
+from app.serializers import MovieSchema
 
 
 api = Blueprint('api', __name__)
@@ -6,3 +8,9 @@ api = Blueprint('api', __name__)
 @api.route('/')
 def status():
     return 'Hello World'
+
+@api.route('/movies', methods=['GET'])
+def movies():
+    movie = Movie.query.all()
+    movie_schema = MovieSchema()
+    return movie_schema.jsonify(movie, many=True)
--- app/serializers.py
+++ app/serializers.py
@@ -1,4 +1,18 @@
 from flask_marshmallow import Marshmallow
+from app.models import Movie, Cast
 
 
 ma = Marshmallow()
+
+
+class CastSchema(ma.Schema):
+    class Meta:
+        fields = ('role', 'name')
+
+
+class MovieSchema(ma.ModelSchema):
+    class Meta:
+        model = Movie
+
+    casts = ma.Nested(CastSchema, many=True)
+
